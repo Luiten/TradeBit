2c39c3d83dc5b742935db7a4b0e8a0a36dd6c149
Luiten <power2845@naver.com>
2018-02-23 03:42:33 +0900

    Practice #2 - MahApps Metro UI 사용

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,22 +12,27 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace TradeBit
 {
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
+            int nTier = (System.Windows.Media.RenderCapability.Tier >> 16);
+            MessageBox.Show(nTier.ToString());
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("TEST!");
+            Window f = new Window();
+           // f.Parent.SetValue(this);
+            f.Show();
         }
 
         private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
